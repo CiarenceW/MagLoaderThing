515a4210cf84abf95f30dd567db30875cc7a52eb
Szikaka-97 <michalmuszka@interia.pl>
2023-01-05 15:08:50 +0100

    Created a version of MagazineLoader class that rapidly loads bullets and made the game use it instead of original

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 using Receiver2;
+using System;
 
 namespace MagLoaderThing
 {
@@ -18,6 +19,13 @@ namespace MagLoaderThing
 
             magloader_enabled = Config.Bind("Magloader settings", "Enable the magloader", true, "Should the magloader appear in the Compound");
 
+			magloader_enabled.SettingChanged += (object sender, EventArgs args) => {
+				if (shooting_range_magloader != null) shooting_range_magloader.SetActive(magloader_enabled.Value);
+				if (challenge_dome_magloader != null) challenge_dome_magloader.SetActive(magloader_enabled.Value);
+
+				Debug.Log("Enabled");
+			};
+
             Harmony.CreateAndPatchAll(this.GetType());
         }
 
@@ -25,13 +33,22 @@ namespace MagLoaderThing
         [HarmonyPostfix]
         private static void OnStartCompound() {
             if (ReceiverCoreScript.Instance().game_mode.GetGameMode() != GameMode.ReceiverMall) return;
-            shooting_range_magloader = GameObject.Find("/Shooting Range/Gameplay/Ammo Tables/MagazineLoader");
-            challenge_dome_magloader = GameObject.Find("/Challenge Room/Challenge Room Geometry/AmmoTable/MagazineLoader");
-        }
 
-        private void Update() {
-            if (shooting_range_magloader != null) shooting_range_magloader.SetActive(magloader_enabled.Value);
-            if (challenge_dome_magloader != null) challenge_dome_magloader.SetActive(magloader_enabled.Value);
-        }
+            shooting_range_magloader = GameObject.Find("/Shooting Range/Gameplay/Ammo Tables/MagazineLoader");
+			if (shooting_range_magloader != null) {
+				shooting_range_magloader.SetActive(magloader_enabled.Value);
+
+				DestroyImmediate(shooting_range_magloader.transform.Find("collider").GetComponent<MagazineLoader>());
+				shooting_range_magloader.transform.Find("collider").gameObject.AddComponent<AutomaticMagLoader>();
+			}
+
+            challenge_dome_magloader = GameObject.Find("/Challenge Room/Challenge Room Geometry/AmmoTable/MagazineLoader");
+			if (challenge_dome_magloader != null) {
+				challenge_dome_magloader.SetActive(magloader_enabled.Value);
+
+				DestroyImmediate(challenge_dome_magloader.transform.Find("collider").GetComponent<MagazineLoader>());
+				challenge_dome_magloader.transform.Find("collider").gameObject.AddComponent<AutomaticMagLoader>();
+			}
+		}
     }
 }
